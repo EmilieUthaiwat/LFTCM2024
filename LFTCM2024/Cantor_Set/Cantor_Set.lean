f455850b5042ac555c4744ff0839c2ba96e35ddb
Tomas Jakl <tomas@jakl.one>
2024-03-25 22:15:09 +0100

    correction

--- LFTCM2024/Cantor_Set/Cantor_Set.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set.lean
@@ -8,6 +8,6 @@ def T_R (x : ℝ) : ℝ := (2+x)/3
 
 def pre_Cantor_set : ℕ → Set ℝ
   | 0 => Set.Icc 0 1
-  | Nat.succ n => T_L '' pre_Cantor_set (n - 1) ∪ T_R '' pre_Cantor_set (n - 1)
+  | Nat.succ n => T_L '' pre_Cantor_set n ∪ T_R '' pre_Cantor_set n
 
 def Cantor_set := iInf pre_Cantor_set
