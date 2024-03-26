9e1441385600f92a6e73c0587c9f8cb901cc4340
Isabelle-Dubois <isabelle.dubois@univ-lorraine.fr>
2024-03-26 12:10:40 +0100

    visa

--- LFTCM2024/Cantor_Set/Cantor_Set_team3_isa.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set_team3_isa.lean
@@ -1,7 +1,7 @@
 import LFTCM2024.Cantor_Set.Cantor_Team_3
 import Mathlib.Topology.MetricSpace.Basic
 
-instance Cantor_set.metricSpace : MetricSpace Cantor_set :=
-  Subtype.metricSpace
+-- instance Cantor_set.metricSpace : MetricSpace Cantor_set :=
+--  Subtype.metricSpace
 
 #check Cantor_set.metricSpace
