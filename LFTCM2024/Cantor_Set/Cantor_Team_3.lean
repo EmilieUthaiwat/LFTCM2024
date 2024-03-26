5bb87c661f8b12ebb72d547e76232f3b9373aea2
Artur Szafarczyk <artur@szafarczyk.pl>
2024-03-26 18:05:34 +0100

    Copied Homeomorph T_L and T_R from Artur to Cantor_Team_3

--- LFTCM2024/Cantor_Set/Cantor_Team_3.lean
+++ LFTCM2024/Cantor_Set/Cantor_Team_3.lean
@@ -4,6 +4,47 @@ import Mathlib.Topology.Metrizable.Basic
 import Mathlib.Topology.Defs.Basic
 import Mathlib.Topology.Connected.TotallyDisconnected
 import Mathlib.Topology.Perfect
+import Mathlib.Analysis.Calculus.ContDiff.Basic
+
+noncomputable def Homeomorph_T_L : Homeomorph ℝ ℝ where
+  toFun := T_L
+  invFun := fun x ↦ 3*x
+  left_inv := by
+    intro x
+    unfold T_L
+    simp only
+    ring
+  right_inv := by
+    intro x
+    unfold T_L
+    simp only
+    ring
+  continuous_toFun := by
+    unfold T_L
+    simp only
+    continuity
+  continuous_invFun := by
+    continuity
+
+noncomputable def Homeomorph_T_R : Homeomorph ℝ ℝ where
+  toFun := T_R
+  invFun := fun x ↦ 3*x - 2
+  left_inv := by
+    intro x
+    unfold T_R
+    simp only
+    ring
+  right_inv := by
+    intro x
+    unfold T_R
+    simp only
+    ring
+  continuous_toFun := by
+    unfold T_R
+    simp only
+    continuity
+  continuous_invFun := by
+    continuity
 
 instance Cantor_set.metricSpace : MetricSpace Cantor_set :=
   Subtype.metricSpace
