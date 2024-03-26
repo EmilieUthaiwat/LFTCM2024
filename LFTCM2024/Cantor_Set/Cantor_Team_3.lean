6196637fc2907c5bd64008d8ff11673d494c05da
Lorenzo <lollypop3013@gmail.com>
2024-03-26 11:32:47 +0100

    fixed python to lean

---
+++ LFTCM2024/Cantor_Set/Cantor_Team_3.lean
@@ -0,0 +1,2 @@
+import LFTCM2024.Cantor_Set.Cantor_Set
+import Mathlib.Topology.Defs.Basic
---
+++ LFTCM2024/Cantor_Set/Lorenzo.Zanichelli.lean
@@ -0,0 +1,21 @@
+import LFTCM2024.Cantor_Set.Cantor_Set
+import Mathlib.Topology.Defs.Basic
+
+lemma Cantor_Set_Closed : IsClosed Cantor_Set  := by
+  sorry
+
+lemma Cantor_Set_Compact : := by
+  sorry
+
+lemma Cantor_Set_T2 : := by
+  sorry
+
+
+lemma Cantor_Set_metrizable : := by
+  sorry
+
+lemma Cantor_Set_Perfect : := by --no isolated points
+  sorry
+
+lemma Cantor_Set_Tot_Disc : := by
+  sorry
