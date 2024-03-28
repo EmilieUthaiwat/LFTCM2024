16148d0359475e5296be78bd4eb5501bccfa69eb
JanaGoeken <goeken@uni-bremen.de>
2024-03-28 15:14:53 +0100

    last 5 sorrys of totally disconnectedness done

--- LFTCM2024/Cantor_Set/Cantor_Team_3.lean
+++ LFTCM2024/Cantor_Set/Cantor_Team_3.lean
@@ -99,7 +99,7 @@ lemma Cantor_set_preperfect : Preperfect Cantor_set := by
  --no isolated points
   sorry
 
-lemma Cantor_set_tot_disc : TotallyDisconnectedSpace Cantor_set := by
+lemma Cantor_set_tot_disc' : TotallyDisconnectedSpace Cantor_set := by
   apply (totallyDisconnectedSpace_iff Cantor_set).2
   intro S hS h₁S x h₁x y h₁y
   by_contra nhxy
