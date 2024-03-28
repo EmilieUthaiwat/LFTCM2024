8b732f978534bb750285b77ddf87c25df66888a9
Emilie Uthaiwat <emiliepathum@gmail.com>
2024-03-28 12:21:52 +0100

    update

--- LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
@@ -4,29 +4,26 @@ import LFTCM2024.Cantor_Set.Cantor_Set
 def Cantor_set_union_Icc (n k : ℕ) : Set ℝ :=
   Set.Icc ((3*k)/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)
 
-def f (n : ℕ) : Set.Icc 0 (3^(n-1)-1) → Set ℝ :=
-  fun k => Cantor_set_union_Icc n k
+def f (n : ℕ) (k : ℕ) (_ : k ≤ 3^(n-1)-1) : Set ℝ :=
+  Cantor_set_union_Icc n k
 
-def Cantor_set_Union_Icc (n : ℕ) := iSup (f n)
+def Cantor_set_Union_Icc (n : ℕ) := ⋃ (k : ℕ) (hk : k ≤ 3^(n-1)-1), f n k hk
 
-/- The function g takes entries from [0,∞)-/
-def g : Set.Ici 1 → Set ℝ := fun n => Cantor_set_Union_Icc n
+/- The function g takes entries from [1,∞) -/
+def g (i : ℕ) (_ : 1 ≤ i) : Set ℝ := Cantor_set_Union_Icc i
 
-def Cantor_set_Icc := iInf g
+def Cantor_set_Icc := ⋂ (i : ℕ) (hi : 1 ≤ i), g i hi
 
-def h (n : ℕ) : Set.Icc 0 n → Set ℝ := fun l => Cantor_set_Union_Icc l
+def h (n : ℕ) (i : ℕ) (_ : i ≤ n) : Set ℝ := Cantor_set_Union_Icc i
 
-def C'' : ℕ → Set ℝ :=
-  fun n => (Cantor_set_Union_Icc n ∩ Cantor_set_Union_Icc (n-1)) ∪ iInf (h n)
+def C'' (n : ℕ) : Set ℝ := ⋂ (i : ℕ) (hi : i ≤ n), h n i hi
 
-theorem Cantor_set_Union_Icc_subset :
-    ∀ n, Cantor_set_Union_Icc n ⊆ Cantor_set_Union_Icc (n+1) := by
-  intro n
-  unfold Cantor_set_Union_Icc
-  unfold f
-  rw [Set.iSup_eq_iUnion, Set.iSup_eq_iUnion]
+theorem Cantor_set_Union_Icc_subset_C'' (n : ℕ) : C'' n ⊆ Cantor_set_Union_Icc n := by
+  refine' Set.iInter₂_subset n _
+  trivial
 
-theorem Cantor_set_Union_Icc_subset_C'' (n : ℕ) : C'' (n+1) ⊆ Cantor_set_Union_Icc (n+1) := by
+theorem Cantor_set_Union_Icc_eq_pre_Cantor_set (n : ℕ) (hn : 1 ≤ n) :
+    Cantor_set_Union_Icc n = pre_Cantor_set n := by
   sorry
 
 theorem Cantor_set_eq_Icc : Cantor_set = Cantor_set_Icc := by
