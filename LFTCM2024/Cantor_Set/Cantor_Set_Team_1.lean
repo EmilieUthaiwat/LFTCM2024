25e69f5e8154a5ce132438669097f30b43cf50df
SurajKrishnaMS <surajkms@gmail.com>
2024-03-28 09:32:56 +0100

    Added two docstrings

--- LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
@@ -1,14 +1,15 @@
 import LFTCM2024.Cantor_Set.Cantor_Set
 
+/- Function which takes n and k as input and gives the union of two closed intervals as output-/
 def Cantor_set_union_Icc (n k : ℕ) : Set ℝ :=
   Set.Icc (3*k/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)
-  -- Set.Icc ((3*k)/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)
 
 def f (n : ℕ) : Set.Icc 0 (3^(n-1)-1) → Set ℝ :=
   fun k => Cantor_set_union_Icc n k
 
 def Cantor_set_Union_Icc (n : ℕ) := iSup (f n)
 
+/- The function g takes entries from [0,∞)-/
 def g : Set.Ici 1 → Set ℝ := fun n => Cantor_set_Union_Icc n
 
 def Cantor_set_Icc := iInf g
