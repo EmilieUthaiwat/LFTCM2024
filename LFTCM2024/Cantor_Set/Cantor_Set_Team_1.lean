1cb497377a066455392ef3025147f3e37cb4e5db
Emilie Uthaiwat <emiliepathum@gmail.com>
2024-03-28 09:10:09 +0100

    correction

--- LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
@@ -1,7 +1,7 @@
 import LFTCM2024.Cantor_Set.Cantor_Set
 
 def Cantor_set_union_Icc (n k : ℕ) : Set ℝ :=
-  Set.Icc (3*k/3^n) (3*k+1/3^n) ∪ Set.Icc (3*k+2/3^n) (3*k+3/3^n)
+  Set.Icc ((3*k)/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)
 
 def f (n : ℕ) : Set.Icc 0 (3^(n-1)-1) → Set ℝ :=
   fun k => Cantor_set_union_Icc n k
@@ -20,7 +20,7 @@ def C'' : ℕ → Set ℝ :=
 theorem Cantor_set_Union_Icc_subset : ∀ n, Cantor_set_Union_Icc n ⊆ Cantor_set_Union_Icc (n+1) := by
   sorry
 
-theorem Cantor_set_Union_Icc_subset_C'' (n : ℕ) : Cantor_set_Union_Icc (n+1) ⊆ C'' (n+1) := by
+theorem Cantor_set_Union_Icc_subset_C'' (n : ℕ) : C'' (n+1) ⊆ Cantor_set_Union_Icc (n+1) := by
   sorry
 
 theorem Cantor_set_eq_Icc : Cantor_set = Cantor_set_Icc := by
