f2ab3b7ad806d910d1d847f02cab713d9bd7f12a
SurajKrishnaMS <surajkms@gmail.com>
2024-03-28 09:12:01 +0100

    brackets added

--- LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set_Team_1.lean
@@ -1,7 +1,7 @@
 import LFTCM2024.Cantor_Set.Cantor_Set
 
 def Cantor_set_union_Icc (n k : ℕ) : Set ℝ :=
-  Set.Icc (3*k/3^n) (3*k+1/3^n) ∪ Set.Icc (3*k+2/3^n) (3*k+3/3^n)
+  Set.Icc (3*k/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)
 
 def f (n : ℕ) : Set.Icc 0 (3^(n-1)-1) → Set ℝ :=
   fun k => Cantor_set_union_Icc n k
