77eb6e46e1d6b7506ccc7d080f7c8b82776606da
Lorenzo <lollypop3013@gmail.com>
2024-03-29 01:44:19 +0100

    added proof of Cantor IsPerfect

--- LFTCM2024/Cantor_Set/Cantor_Set.lean
+++ LFTCM2024/Cantor_Set/Cantor_Set.lean
@@ -193,3 +193,112 @@ lemma Cantor_set_T2 : T2Space Cantor_set := by
   infer_instance
 lemma Cantor_set_metrizable : TopologicalSpace.MetrizableSpace Cantor_set := by
   infer_instance
+
+--
+--Proof of Cantor_set IsPerfect
+--
+
+lemma cipher (m n b : ℕ )(hb: b>1) (hm : n≤m) : ∀ x : ℝ, 0 ≤ x →  ((Nat.floor (x*(b^m)))/(b^m):ℝ)∈
+Set.Icc (((Nat.floor (x*b^n):ℝ)/(b^n))) (((Nat.ceil (x*b^n): ℝ)/(b^n))) := by
+simp
+intro x hx
+constructor
+· have : (⌊x * ↑b ^ n⌋₊:ℝ) / b ^ n = (⌊x * ↑b ^ n⌋₊*b^(m-n)) / (b ^ n*b^(m-n)) := by
+    rw [mul_div_mul_right]
+    positivity
+  rw[this, ← pow_add, Nat.add_sub_cancel' hm]
+  gcongr
+  norm_cast
+  suffices ⌊x * ↑b ^ n⌋₊*b^(m-n)≤ x*b^m by
+    rw[Nat.le_floor_iff]
+    exact_mod_cast this
+    positivity      --_ ≤ _ := sorry
+  obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le hm
+  simp only [add_comm n k, add_tsub_cancel_right, pow_add]
+  rw [mul_comm ((b:ℝ)^k:ℝ), ← mul_assoc]
+  gcongr
+  apply Nat.floor_le
+  positivity
+· have : (⌈x * ↑b ^ n⌉₊:ℝ) / b ^ n = (⌈x * ↑b ^ n⌉₊*b^(m-n)) / (b ^ n*b^(m-n)) := by
+    rw [mul_div_mul_right]
+    positivity
+  rw[this, ← pow_add, Nat.add_sub_cancel' hm]
+  gcongr
+  norm_cast
+  apply le_trans (Nat.floor_le_ceil _)
+  suffices ⌈x * ↑b ^ n⌉₊*b^(m-n)≥ x*b^m by
+    rw[Nat.ceil_le]
+    exact_mod_cast this
+  obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le hm
+  simp only [add_comm n k, add_tsub_cancel_right, pow_add]
+  rw [mul_comm ((b:ℝ)^k:ℝ), ← mul_assoc]
+  gcongr
+  apply Nat.le_ceil
+
+lemma foo (hx : x ∈  Cantor_set) (n m :ℕ) (hm: m<n):
+(((Nat.floor (x*3^n):ℝ)/3^n))∈ Set.Icc (((Nat.floor (x*3^m):ℝ)/3^m)) (((Nat.ceil (x*3^m):ℝ)/3^m)) := by
+apply cipher
+· simp
+· exact hm.le
+· have : Cantor_set ⊆ Set.Icc 0 1 := by
+    unfold Cantor_set
+    intro x hx
+    simp only [Set.iInf_eq_iInter, Set.mem_iInter] at hx
+    exact hx 0
+  suffices x∈ Set.Icc 0 1 by
+    exact this.1
+  exact this hx
+
+lemma bar {n :ℕ}(m: ℕ)(hx : x ∈  Cantor_set) (hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
+Set.Icc (((Nat.floor (x*3^m):ℝ)/3^m)) (((Nat.ceil (x*3^m):ℝ)/3^m))⊆ pre_Cantor_set_Icc m := by
+sorry
+
+lemma extremuses_of_Cantor_set_nice_x  (hx : x ∈  Cantor_set) (n :ℕ) (hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
+(((Nat.floor (x*3^n))/3^n):ℝ)∈ Cantor_set := by--∧ (((Nat.ceil (x.1*3^n))/3^n):ℝ) ∈ Cantor_set :=
+suffices h1:  ∀ m :ℕ,  ((((Nat.floor (x*3^n))/3^n):ℝ)∈ pre_Cantor_set_Icc m) by
+  have : (⌊x * 3 ^ n⌋₊:ℝ) / 3 ^ n ∈ Cantor_set_Icc := by
+    sorry --waiting for Cantor_set_Icc_mem............
+  -- apply Cantor_set_eq_Icc
+  sorry
+intro m
+by_cases hm : m ≥ n
+· sorry
+· push_neg at hm
+  suffices (((Nat.floor (x*3^n))/3^n):ℝ)∈ Set.Icc (((Nat.floor (x*3^m))/3^m):ℝ) (((Nat.ceil (x*3^m))/3^m):ℝ) by
+    apply bar m hx hnice
+    exact this
+  apply foo <;> assumption
+
+
+lemma LZCantor_set_preperfect : Preperfect Cantor_set := by
+rw [preperfect_iff_nhds]
+intro x hx U hU
+rw [ Metric.mem_nhds_iff] at hU
+obtain ⟨ ε , epos, hball ⟩ := hU
+let n : ℕ  := (Nat.ceil (Real.logb 3 (1/ε)))+1
+have hnε : 3^(-(n:ℤ))<ε := by
+        simp
+        sorry  --calculations....
+by_cases hnice : ∀ a<n, (x≠ (a:ℝ)/3^n)
+· set y : ℝ := ((Nat.floor (x*3^n))/3^n:ℝ) with hy1
+  --by_cases hy : y≠ x
+  have : y∈ U∩ Cantor_set := by
+    constructor
+    · apply hball
+      simp
+      suffices x-y≤ (1/3^n) by
+        sorry
+      rw[hy1]
+      rw[le_div_iff]
+      sorry --calculations.......
+      --rw[right_distrib]
+      positivity
+    · apply extremuses_of_Cantor_set_nice_x
+      assumption
+      assumption
+  use y
+  constructor
+  · exact this
+  · sorry
+· push_neg at hnice
+  sorry
--- LFTCM2024/Cantor_Set/Lorenzo.Zanichelli.lean
+++ LFTCM2024/Cantor_Set/Lorenzo.Zanichelli.lean
@@ -1,143 +1,119 @@
-import LFTCM2024.Cantor_Set.Cantor_Team_3
-import LFTCM2024.Cantor_Set.Cantor_Set_Team_1
--- --lemma Cantor_set_preperfect : Preperfect Cantor_set := by --no isolated points
---   intro x
---   intro hx
---   let ε : ℝ
---     let n : ℕ, n>
---   by
---     done
--- lemma extremuses_of_Cantor_set (n:ℕ) (k:ℕ) (i:ℕ) (h : k≤ 3^(n-1)-1)(hi : i≤ 3) :
---   ((((k+i) : ℝ)/(3^n)) ∈ Cantor_set) := by
---     suffices this : ∀ m : ℕ, (((k+i) : ℝ)/(3^n))∈ pre_Cantor_set m by
---       simp [Cantor_set, this]
---     intro m
---     induction m with
---     | zero =>
---         unfold pre_Cantor_set
---         simp
---         constructor
---         · positivity
---         · refine (div_le_one ?_).mpr ?_
---           positivity
---           norm_cast
+import LFTCM2024.Cantor_Set.Cantor_Set
 
---     | succ n ih =>
---       --((k : ℝ)/(3^n))
---       unfold pre_Cantor_set
---       r (hL , hR)
---       sorry
-
--- lemma Ceil_minus_one_aux (x : ℝ) : |⌈-x⌉ + x| < 1 := by
---   rw [Int.ceil_neg, add_comm, Int.cast_neg]
---   apply Int.abs_sub_lt_one_of_floor_eq_floor
---   simp
-
--- lemma Ceil_minus_one (x : ℝ) : |⌈x⌉ - x| < 1 := by
---   have h : _ := Ceil_minus_one_aux (- x)
---   simpa using h
-lemma cipher (m n b : ℕ )(hb: b>1) (hm : n≤m) : ∀x:ℝ, (Nat.ceil (x*(b^m)))/(b^m)∈
-Set.Icc ((Nat.ceil (x*b^n))/(b^n)) ((Nat.ceil (x*b^n)+1)/(b^m)) := by
+lemma cipher (m n b : ℕ )(hb: b>1) (hm : n≤m) : ∀ x : ℝ, 0 ≤ x →  ((Nat.floor (x*(b^m)))/(b^m):ℝ)∈
+Set.Icc (((Nat.floor (x*b^n):ℝ)/(b^n))) (((Nat.ceil (x*b^n): ℝ)/(b^n))) := by
 simp
-sorry
+intro x hx
+constructor
+· have : (⌊x * ↑b ^ n⌋₊:ℝ) / b ^ n = (⌊x * ↑b ^ n⌋₊*b^(m-n)) / (b ^ n*b^(m-n)) := by
+    rw [mul_div_mul_right]
+    positivity
+  rw[this, ← pow_add, Nat.add_sub_cancel' hm]
+  gcongr
+  norm_cast
+  suffices ⌊x * ↑b ^ n⌋₊*b^(m-n)≤ x*b^m by
+    rw[Nat.le_floor_iff]
+    exact_mod_cast this
+    positivity      --_ ≤ _ := sorry
+  obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le hm
+  simp only [add_comm n k, add_tsub_cancel_right, pow_add]
+  rw [mul_comm ((b:ℝ)^k:ℝ), ← mul_assoc]
+  gcongr
+  apply Nat.floor_le
+  positivity
+· have : (⌈x * ↑b ^ n⌉₊:ℝ) / b ^ n = (⌈x * ↑b ^ n⌉₊*b^(m-n)) / (b ^ n*b^(m-n)) := by
+    rw [mul_div_mul_right]
+    positivity
+  rw[this, ← pow_add, Nat.add_sub_cancel' hm]
+  gcongr
+  norm_cast
+  apply le_trans (Nat.floor_le_ceil _)
+  suffices ⌈x * ↑b ^ n⌉₊*b^(m-n)≥ x*b^m by
+    rw[Nat.ceil_le]
+    exact_mod_cast this
+  obtain ⟨k, rfl⟩ := Nat.exists_eq_add_of_le hm
+  simp only [add_comm n k, add_tsub_cancel_right, pow_add]
+  rw [mul_comm ((b:ℝ)^k:ℝ), ← mul_assoc]
+  gcongr
+  apply Nat.le_ceil
 
-lemma foo (hx : x ∈  Cantor_set) (n m :ℕ) (hm: m<n)(hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
-(((Nat.floor (x*3^n))/3^n):ℝ)∈ Set.Icc (((Nat.floor (x*3^m))/3^m):ℝ) ((((Nat.floor (x*3^m))+1)/3^m):ℝ) := by
+lemma foo (hx : x ∈  Cantor_set) (n m :ℕ) (hm: m<n):
+(((Nat.floor (x*3^n):ℝ)/3^n))∈ Set.Icc (((Nat.floor (x*3^m):ℝ)/3^m)) (((Nat.ceil (x*3^m):ℝ)/3^m)) := by
 apply cipher
-sorry
+· simp
+· exact hm.le
+· have : Cantor_set ⊆ Set.Icc 0 1 := by
+    unfold Cantor_set
+    intro x hx
+    simp only [Set.iInf_eq_iInter, Set.mem_iInter] at hx
+    exact hx 0
+  suffices x∈ Set.Icc 0 1 by
+    exact this.1
+  exact this hx
 
-lemma bar (hx : x ∈  Cantor_set) (Cantor_set_metrizable :ℕ) (hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
-Set.Icc (((Nat.floor (x*3^m))/3^m):ℝ) (((Nat.floor (x*3^m)+1)/3^m):ℝ)⊆ pre_Cantor_set_Icc m := by
+lemma bar {n :ℕ}(m: ℕ)(hx : x ∈  Cantor_set) (hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
+Set.Icc (((Nat.floor (x*3^m):ℝ)/3^m)) (((Nat.ceil (x*3^m):ℝ)/3^m))⊆ pre_Cantor_set_Icc m := by
 sorry
 
 lemma extremuses_of_Cantor_set_nice_x  (hx : x ∈  Cantor_set) (n :ℕ) (hnice : ∀ a<n, x≠ (a:ℝ)/3^n) :
-(((Nat.floor (x*3^n))/3^n):ℝ)∈ Cantor_set :=--∧ (((Nat.ceil (x.1*3^n))/3^n):ℝ) ∈ Cantor_set :=
-by
-suffices h1:  ∀ m :ℕ,  ((((Nat.floor (x*3^n))/3^n):ℝ)∈ pre_Cantor_set_Icc m) from by
+(((Nat.floor (x*3^n))/3^n):ℝ)∈ Cantor_set := by--∧ (((Nat.ceil (x.1*3^n))/3^n):ℝ) ∈ Cantor_set :=
+suffices h1:  ∀ m :ℕ,  ((((Nat.floor (x*3^n))/3^n):ℝ)∈ pre_Cantor_set_Icc m) by
+  have : (⌊x * 3 ^ n⌋₊:ℝ) / 3 ^ n ∈ Cantor_set_Icc := by
+    sorry --waiting for Cantor_set_Icc_mem............
+  -- apply Cantor_set_eq_Icc
   sorry
 intro m
 by_cases hm : m ≥ n
 · sorry
 · push_neg at hm
-  suffices (((Nat.floor (x*3^n))/3^n):ℝ)∈ Set.Icc (((Nat.floor (x*3^m))/3^m):ℝ) (((Nat.floor (x*3^m))/3^m):ℝ) from
-    sorry
-  apply foo
-  assumption
-  assumption
-  assumption
-noncomputable
-def function_extremuses_Cantor_set(n:ℕ):  Cantor_set → Cantor_set :=
-  fun x =>
-  if (((Nat.floor (x.1*3^n))/3^n):ℝ)==x
-    then sorry
-  else
-    have p : ∃ k≤ 3^(n-1)-1, x∈ (Set.Icc ((3*k)/3^n) ((3*k+1)/3^n) ∪ Set.Icc ((3*k+2)/3^n) ((3*k+3)/3^n)) := by
-      sorry
-    let h := p.1
-  ⟨(3*h)/3^n⟩
-  sorry
-
-lemma extremuses_of_Cantor_set  (x : Cantor_set) (n :ℕ) :
-(((Nat.floor (x.1*3^n))/3^n):ℝ)∈ Cantor_set∧ (((Nat.ceil (x.1*3^n))/3^n):ℝ) ∈ Cantor_set :=
-by
-  sorry
-  -- suffices hfin : ∀ m ∈ ℕ,
-  -- ((Nat.ceil (x.1*3^n))/3^n) ∈ Cantor_set_Union_Icc m
-  -- ∧ ((Nat.ceil (x.1*3^n)-(1) : ℕ)/3^n) ∈ Cantor_set_Union_Icc m by
-  --   intro m
-  --   sorry
-
-
-
-  -- --(((Nat.ceil (x.1*3^n))/3^n):ℝ)∈ Cantor_set∧ (((Nat.floor (x.1*3^n) : ℕ)/3^n):ℝ) ∈
-
-  -- have Cantor_set : Set ℝ := by
-  --   sorry
-  -- have hk :  ∃ k : ℕ ,
-  -- (x∈ ((Set.Icc (3*k/3^n) (3*k+1/3^n) ∪ Set.Icc (3*k+2/3^n) (3*k+3/3^n)) : Set ℝ)) := by
-  --   sorry
-  -- rintro hk with (hk1 | hk2)
-
-
-  -- sorry
-
-
+  suffices (((Nat.floor (x*3^n))/3^n):ℝ)∈ Set.Icc (((Nat.floor (x*3^m))/3^m):ℝ) (((Nat.ceil (x*3^m))/3^m):ℝ) by
+    apply bar m hx hnice
+    exact this
+  apply foo <;> assumption
 
 
 lemma LZCantor_set_preperfect : Preperfect Cantor_set := by
-  rw [preperfect_iff_nhds]
-
-  intro x hx U hU
-  rw [ Metric.mem_nhds_iff] at hU
-  obtain ⟨ ε , epos, hball ⟩ := hU
-  let n : ℕ  := (Nat.ceil (Real.logb 3 (1/ε)))+1
-  have hnε : 3^(-(n:ℤ))<ε := by
-          simp
-          sorry  --calculations....
-  set y : ℝ := ((Nat.floor (x*3^n))/3^n) with hy1
-  by_cases hy : y≠ x
-  · have : y∈ U∩ Cantor_set := by
-      constructor
-      · apply hball
+rw [preperfect_iff_nhds]
+intro x hx U hU
+rw [ Metric.mem_nhds_iff] at hU
+obtain ⟨ ε , epos, hball ⟩ := hU
+let n : ℕ  := (Nat.ceil (Real.logb 3 (1/ε)))+1
+have hnε : 3^(-(n:ℤ))<ε := by
         simp
-
-        apply lt_trans ?_ ineq
-        rw[hy1]
-
-        --Int.ceil_lt_add_one  --calculations...
+        sorry  --calculations....
+by_cases hnice : ∀ a<n, (x≠ (a:ℝ)/3^n)
+· set y : ℝ := ((Nat.floor (x*3^n))/3^n:ℝ) with hy1
+  --by_cases hy : y≠ x
+  have : y∈ U∩ Cantor_set := by
+    constructor
+    · apply hball
+      simp
+      suffices x-y≤ (1/3^n) by
         sorry
-      · apply (extremuses_of_Cantor_set ⟨x, hx⟩ n).2
-    use y
-  · let z : ℝ := ((Nat.floor (x*3^n))/3^n)  --(Nat.ceil (x*3^n)-(1) : ℕ)/3^n
-    have : z∈ U∩ Cantor_set := by
-      constructor
-      · apply hball
-        simp
-        sorry --calculation...
-      · apply (extremuses_of_Cantor_set ⟨x, hx⟩ n).1
+      rw[hy1]
+      rw[le_div_iff]
+      sorry --calculations.......
+      --rw[right_distrib]
+      positivity
+    · apply extremuses_of_Cantor_set_nice_x
+      assumption
+      assumption
+  use y
+  constructor
+  · exact this
+  · sorry
+· push_neg at hnice
+  sorry
+  -- · let z : ℝ := ((Nat.floor (x*3^n))/3^n:ℝ)  --(Nat.ceil (x*3^n)-(1) : ℕ)/3^n
+  --   have : z∈ U∩ Cantor_set := by
+  --     constructor
+  --     · apply hball
+  --       simp
+  --       sorry --calculation...
+  --     · apply (extremuses_of_Cantor_set ⟨x, hx⟩ n).1
 
-    have : z≠ x := by
-      push_neg at hy
-      rw[hy.symm]
-      sorry
-    use z
+  --   have : z≠ x := by
+  --     push_neg at hy
+  --     rw[hy.symm]
+  --     sorry
+  --   use z
